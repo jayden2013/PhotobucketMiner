d15498d7f0b034e5039d84bb1477867f3f480c6e
jayden2013 <jay-weav@hotmail.com>
2017-09-19 21:45:28 -0600

    Fixed Photobucket Status Check
    Added a different website to check the status of Photobucket because the
    other one was having problems. In the future, maybe multiple sites will
    be used to check.

--- PBisDown.java
+++ PBisDown.java
@@ -11,13 +11,13 @@ import org.jsoup.select.Elements;
  */
 public class PBisDown {
 
-	private String url = "http://isitdownorjust.me/photobucket-com/"; //Website to use to check if Photobucket is down.
+	private String url = "http://downforeveryoneorjustme.com/photobucket.com"; //Website to use to check if Photobucket is down.
 	private boolean isDown = false;
 
 	public PBisDown() {
 		try {
-			Document photobucket = Jsoup.connect(url).timeout(10000).get();
-			Elements elements = photobucket.getElementsContainingText("Photobucket.com seems to be down.");
+			Document photobucket = Jsoup.connect(this.url).timeout(10000).get();
+			Elements elements = photobucket.getElementsContainingText("It's not just you!");
 			this.isDown = !elements.isEmpty();
 		} catch (IOException e) {
 			System.err.println("Unable to verify status of Photobucket. Failed to connect to remote server.");
